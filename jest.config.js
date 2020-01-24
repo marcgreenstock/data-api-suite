365a9c728f7b598b420760db6cc9213e0a11b71b
Marc Greenstock <marc@marcgreenstock.com>
2020-01-24 17:16:10 +0100

    remove packages from jest

--- jest.config.js
+++ jest.config.js
@@ -4,28 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: [
-    'packages/**/src/**/*.ts',
-    '!packages/**/src/**/*.test.ts'
-  ],
-  projects: [{
-    displayName: 'AuroraDataAPI',
-    testMatch: ['<rootDir>/packages/aurora-data-api/src/**/*.test.ts'],
-    preset: 'ts-jest'
-  }, {
-    displayName: 'DataAPILocal',
-    testMatch: ['<rootDir>/packages/data-api-local/src/**/*.test.ts'],
-    preset: 'ts-jest'
-  }, {
-    displayName: 'DataAPILocalServerless',
-    testMatch: ['<rootDir>/packages/data-api-local-serverless/src/**/*.test.ts'],
-    preset: 'ts-jest'
-  }, {
-    displayName: 'DataAPIMigrations',
-    testMatch: ['<rootDir>/packages/data-api-migrations/src/**/*.test.ts'],
-    preset: 'ts-jest'
-  }, {
-    displayName: 'DataAPIMigrationsServerless',
-    testMatch: ['<rootDir>/packages/data-api-migrations-serverless/src/**/*.test.ts'],
-    preset: 'ts-jest'
-  }]
+    'packages/**/src/**/*.ts'
+  ]
 }
